5ea09de773217b5dcf8a4d897611d122f207e723
Juca8 <jccontrerasg08@gmail.com>
2026-04-27 04:38:28 -0600

    fix: populate index.html and remove start.sh for Railpack static detection

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Serve static files using Python's built-in HTTP server.
-# Listens on $PORT if set, otherwise defaults to port 80.
-
-PORT="${PORT:-80}"
-
-echo "Starting HTTP server on port $PORT..."
-exec python3 -m http.server "$PORT" --bind 0.0.0.0
